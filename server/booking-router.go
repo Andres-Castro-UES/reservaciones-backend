af5e97d6e634f5c3db1384fb093b3c0fb37efabf
Andres-Castro-UES <andres.castro@ues.edu.sv>
2024-02-06 14:20:03 -0600

    fix en variables de tiempo

--- server/booking-router.go
+++ server/booking-router.go
@@ -167,7 +167,7 @@ func (router *BookingRouter) getOne(w http.ResponseWriter, r *http.Request) {
 }
 
 func (router *BookingRouter) getAll(w http.ResponseWriter, r *http.Request) {
-	nowTime := time.Now().UTC()
+	//nowTime := time.Now().UTC()
 	//newTime := nowTime.AddDate(0,0,-1)
 	//newTime := nowTime.Add(time.Hour * -8)
 	list, err := GetBookingRepository().GetAllByUser(GetRequestUserID(r), time.Now().UTC())
