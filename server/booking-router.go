3ae9641902054ac10b2b3d594c45121a0db99fc6
Andres-Castro-UES <andres.castro@ues.edu.sv>
2024-02-06 15:19:39 -0600

    correción 2 tiempo UTC

--- server/booking-router.go
+++ server/booking-router.go
@@ -170,9 +170,9 @@ func (router *BookingRouter) getAll(w http.ResponseWriter, r *http.Request) {
 	
 	nowTime := time.Now().UTC()
 	modTime := nowTime.Truncate(time.Hour)
-	//newTime := nowTime.Add(time.Hour * -4)
+	newTime := modTime.Add(time.Hour * -6)
 	//list, err := GetBookingRepository().GetAllByUser(GetRequestUserID(r), time.Now().UTC())
-	list, err := GetBookingRepository().GetAllByUser(GetRequestUserID(r), modTime)
+	list, err := GetBookingRepository().GetAllByUser(GetRequestUserID(r), newTime)
 	if err != nil {
 		log.Println(err)
 		SendInternalServerError(w)
