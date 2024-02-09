e59118187e08adec32a218a89cab22d8382ee92d
Andres-Castro-UES <andres.castro@ues.edu.sv>
2024-02-09 13:00:42 -0600

    server utc hour change

--- server/booking-router.go
+++ server/booking-router.go
@@ -172,7 +172,8 @@ func (router *BookingRouter) getAll(w http.ResponseWriter, r *http.Request) {
 	//nowTime := time.Now().UTC()
 	//modTime := nowTime.Truncate(time.Hour)
 	//newTime := modTime.Add(time.Hour * -6)
-	list, err := GetBookingRepository().GetAllByUser(GetRequestUserID(r), time.Now().UTC())
+	//list, err := GetBookingRepository().GetAllByUser(GetRequestUserID(r), time.Now().UTC())
+	list, err := GetBookingRepository().GetAllByUser(GetRequestUserID(r), time.Now())
 	//list, err := GetBookingRepository().GetAllByUser(GetRequestUserID(r), newTime)
 	if err != nil {
 		log.Println(err)
